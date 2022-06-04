048528b7a243a4ba986171d9056673b6d3663b7b
Fabio Evangelista <fabioevangelista_@hotmail.com>
2022-06-03 23:16:22 -0300

    Criando Service e populando dados no banco.

---
+++ src/main/java/com/evangelista/codeblog/model/service/CodeBlogService.java
@@ -0,0 +1,14 @@
+package com.evangelista.codeblog.model.service;
+
+import com.evangelista.codeblog.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CodeBlogService {
+
+    List<Post> findAll();
+    Optional<Post> findById(long id);
+    Post save(Post post);
+
+}
---
+++ src/main/java/com/evangelista/codeblog/model/service/serviceimpl/CodeBlogServiceImpl.java
@@ -0,0 +1,32 @@
+package com.evangelista.codeblog.model.service.serviceimpl;
+
+import com.evangelista.codeblog.model.Post;
+import com.evangelista.codeblog.model.service.CodeBlogService;
+import com.evangelista.codeblog.repository.CodeBlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CodeBlogServiceImpl implements CodeBlogService {
+
+    @Autowired
+    CodeBlogRepository codeBlogRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return codeBlogRepository.findAll();
+    }
+
+    @Override
+    public Optional<Post> findById(long id) {
+        return codeBlogRepository.findById(id);
+    }
+
+    @Override
+    public Post save(Post post) {
+        return codeBlogRepository.save(post);
+    }
+}
