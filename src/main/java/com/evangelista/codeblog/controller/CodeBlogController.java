6b3fcacebbfb275930e444b8b3619e56d3c1df3d
Fabio Evangelista <fabioevangelista_@hotmail.com>
2022-06-06 18:54:18 -0300

    Criando um método GET com @PathVariable id

--- src/main/java/com/evangelista/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/evangelista/codeblog/controller/CodeBlogController.java
@@ -5,9 +5,11 @@ import com.evangelista.codeblog.model.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -21,6 +23,14 @@ public class CodeBlogController {
         ModelAndView modelAndView = new ModelAndView("posts");
         List<Post> posts = codeBlogService.findAll();
         modelAndView.addObject("posts", posts);
+        return modelAndView;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView modelAndView = new ModelAndView("postDetails");
+        Optional<Post> post = codeBlogService.findById(id);
+        modelAndView.addObject("post", post);
         return modelAndView;
     }
 
