cbee2bf22079f0682146bccf5640f62ecf80a30c
Fabio Evangelista <fabioevangelista_@hotmail.com>
2022-06-05 22:27:51 -0300

    Controller criado

---
+++ src/main/java/com/evangelista/codeblog/controller/CodeBlogController.java
@@ -0,0 +1,27 @@
+package com.evangelista.codeblog.controller;
+
+import com.evangelista.codeblog.model.Post;
+import com.evangelista.codeblog.model.service.CodeBlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+
+@Controller
+public class CodeBlogController {
+
+    @Autowired
+    CodeBlogService codeBlogService;
+
+    @GetMapping("/")
+    public ModelAndView getPosts(){
+        ModelAndView modelAndView = new ModelAndView("posts");
+        List<Post> posts = codeBlogService.findAll();
+        modelAndView.addObject("posts", posts);
+        return modelAndView;
+    }
+
+}
