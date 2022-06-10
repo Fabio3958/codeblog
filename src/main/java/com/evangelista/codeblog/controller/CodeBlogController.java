644c4775c24503c89e824564d1fa2aa411010fbb
Fabio Evangelista <fabioevangelista_@hotmail.com>
2022-06-10 01:05:34 -0300

    Método post e formulário criados

--- src/main/java/com/evangelista/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/evangelista/codeblog/controller/CodeBlogController.java
@@ -4,10 +4,15 @@ import com.evangelista.codeblog.model.Post;
 import com.evangelista.codeblog.model.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +37,22 @@ public class CodeBlogController {
         Optional<Post> post = codeBlogService.findById(id);
         codeBlogService.findById(id).ifPresent(o -> modelAndView.addObject("post", o));
         return modelAndView;
+    }
+
+    @GetMapping("/newpost")
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @PostMapping("/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes redirectAttributes){
+
+        if (result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        codeBlogService.save(post);
+        return "redirect:/posts";
     }
 
 }
